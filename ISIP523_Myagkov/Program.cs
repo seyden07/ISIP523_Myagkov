e3c772fec4c462bc735a728c260a4e70f13be726
SeyDeH <grekov.pavel07@gmail.com>
2025-10-22 08:34:12 +0300

    Добавил функции Сортировки и статистики, переработал работу меню

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Подсчёт потраченных за день средств
+using System.ComponentModel.DataAnnotations;
 using System.Net.Http.Headers;
 
 Console.WriteLine("Введите кол-во операций: ");
@@ -20,27 +21,76 @@ void output(string[] productOrService, int[] cost, int quantity)
 {
     for (int i = 0;i < quantity; i++)
     {
-        Console.WriteLine($"{productOrService[i]} - {cost[i]} руб.");
+        Console.WriteLine($"\n{productOrService[i]} - {cost[i]} руб.");
     }
 }
 
-Console.WriteLine("========= Меню =========");
+void statistics(string[] productOrService, int[] cost, int quantity)
+{
+
+    int average = 0, max = 0, min = 100000, sum = 0;
+
+    for (int i = 0; i < quantity; i++)
+    {
+        average += cost[i];
+        if (min > cost[i]) min = cost[i];
+        if (max < cost[i]) max = cost[i];
+        sum += cost[i];
+    }
+
+    average /= quantity;
+
+    Console.WriteLine($"\nСреднее: {average} \nСумма трат: {sum} \nМинимальная трата: {min} \nМаксимальная трата: {max}");
+}
+
+void BubbleSort(string[] productOrService, int[] cost, int quantity)
+{
+    for (int i = 0; i < quantity; i++)
+    {
+        for (int j = 0; j < quantity - 1; j++)
+        {
+            if (cost[i] < cost[j])
+            {
+                int temp = cost[i];
+                cost[i] = cost[j];
+                cost[j] = temp;
+                string temp2 = productOrService[i];
+                productOrService[i] = productOrService[j];
+                productOrService[j] = temp2;
+            }
+        }
+    }
+}
+
+while (true)
+{
+    Console.WriteLine("\n========= Меню =========");
 Console.WriteLine("1. Вывод данных");
 Console.WriteLine("2. Статистика");
 Console.WriteLine("3. Сортировка по цене");
 Console.WriteLine("4. Конвертация валюты");
 Console.WriteLine("5. Поиск по названию");
+    Console.WriteLine("0. Выход");
 
-for (int i = 0; i < 100000; i++)
-{
+    Console.WriteLine("\nВыберите пункт: ");
     int n = Convert.ToInt32(Console.ReadLine());
-    if (n == 1)
-    {
-        output(productOrService, cost, quantity);
-    }
 
-    if (n == 0)
+    switch (n)
     {
-        break;
+        case 1:
+        output(productOrService, cost, quantity);
+            break;
+        case 2:
+            statistics(productOrService, cost, quantity);
+            break;
+        case 3:
+            BubbleSort(productOrService, cost, quantity);
+            break;
+        case 0:
+            return;
+        default:
+            Console.WriteLine("\nТакого варианта нет!");
+            break;
+    }
     }
 
