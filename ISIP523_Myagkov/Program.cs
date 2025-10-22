22c26e5bede48a072ba6548c51237d3e3d04115b
SeyDeH <grekov.pavel07@gmail.com>
2025-10-22 08:47:37 +0300

    Добавил функции конвертации и поиска, закончил выполнение

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -62,6 +62,80 @@ void BubbleSort(string[] productOrService, int[] cost, int quantity)
     }
 }
 
+void CurrencyConverter(int[] cost, int quantity)
+{
+    Console.WriteLine("\nВыберите в какую валюту хотите перевести: ");
+    Console.WriteLine("1. Доллар");
+    Console.WriteLine("2. Евро");
+    Console.WriteLine("3. Тенге");
+    Console.WriteLine("4. Своя валюта");
+
+    decimal[] costcopy = new decimal[quantity];
+    for (int i = 0; i < quantity; i++)
+    {
+        costcopy[i] = cost[i];
+    }
+
+    int n = Convert.ToInt32(Console.ReadLine());
+    string choice = "";
+    decimal usdRate = 81.35m;
+    decimal eurRate = 94.67m;
+    decimal KZTRate = 0.1512m;
+    decimal perRate = 0;
+
+    if (n == 1)
+    {
+        choice = "USD";
+        for (int i = 0; i < quantity; i++)
+        {
+
+            costcopy[i] = (decimal)(costcopy[i] / usdRate);
+        }
+    }
+    if (n == 2)
+    {
+        choice = "EUR";
+        for (int i = 0; i < quantity; i++)
+        {
+            costcopy[i] = (decimal)(costcopy[i] / eurRate);
+        }
+    }
+    if (n == 3)
+    {
+        choice = "KZT";
+        for (int i = 0; i < quantity; i++)
+        {
+            costcopy[i] = (decimal)(costcopy[i] / KZTRate);
+        }
+    }
+    if (n == 4)
+    {
+        Console.WriteLine("\nВведите курс по отношению к рублю: ");
+        perRate = Convert.ToDecimal(Console.ReadLine());
+        for (int i = 0; i < quantity; i++)
+        {
+            costcopy[i] = (decimal)(costcopy[i] / perRate);
+        }
+    }
+    for (int i = 0; i < quantity; i++)
+    {
+        Console.WriteLine($"\n{productOrService[i]} - {costcopy[i]} {choice}");
+    }
+}
+
+void searchByName(string[] producrOrService, int[] cost, int quantity)
+{
+    Console.WriteLine("\nВведите товар или услугу стоимость которой хотите увидеть: ");
+    string pos = Console.ReadLine();
+    for (int i = 0; i < quantity; i++)
+    {
+        if (pos == producrOrService[i])
+        {
+            Console.WriteLine($"\n{cost[i]}");
+        }
+    }
+}
+
 while (true)
 {
     Console.WriteLine("\n========= Меню =========");
@@ -85,6 +159,12 @@ while (true)
             break;
         case 3:
             BubbleSort(productOrService, cost, quantity);
+            break;
+        case 4:
+            CurrencyConverter(cost, quantity);
+            break;
+        case 5:
+            searchByName(productOrService, cost, quantity);
             break;
         case 0:
             return;
