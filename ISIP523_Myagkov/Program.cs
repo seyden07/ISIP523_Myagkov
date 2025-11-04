c932d550a8af6c2ee4be2948e16067ea7b412034
SeyDeH <grekov.pavel07@gmail.com>
2025-11-05 02:52:41 +0300

    Добавил больше жанров, пересмотрел структуру кода

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -1,33 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-class MainProgramm
+public enum Genre
 {
-    enum Genre
-    {
-        Детектив;
-        Триллер;
-        Комедия;
+    Fantasy,
+    ScienceFiction,
+    Mystery,
+    Romance,
+    Thriller,
+    Biography,
+    History
     }
-    class Book
+
+public class Book
     {
-        public int id;
-        public string name;
-        public string author;
-        public Genre genre;
-        public int year;
-        public int price;
-        List<Book> Library = new List<Book>();
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public Genre Genre { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
 
-        static void Add
+    public override string ToString()
         {
-            Console.WriteLine("Ввдите название произведения, фамилию автора, жанр из предложенных, год написания, цену за книгу");
-            for (int i = 0; i<Genre.length; i++)
-			{
-                Console.WriteLine(Genre[i]);
+        return $"ID: {Id}, Название: {Title}, Автор: {Author}, Жанр: {Genre}, Год: {Year}, Цена: {Price:C}";
 			}
-
-    Library.add
-
 }
+
+class Program
+{
+    static List<Book> books = new List<Book>();
+    static int nextId = 1;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== СИСТЕМА УЧЁТА БИБЛИОТЕКИ ===");
 
     }
 }
