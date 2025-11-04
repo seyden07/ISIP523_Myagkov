a7019aeddff594b3e4c705ce33b7aa61733d8858
SeyDeH <grekov.pavel07@gmail.com>
2025-11-05 02:50:40 +0300

    Подготовил класс Book и написал функцию для добавления книг

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -1,1 +1,33 @@
-﻿
+
+class MainProgramm
+{
+    enum Genre
+    {
+        Детектив;
+        Триллер;
+        Комедия;
+    }
+    class Book
+    {
+        public int id;
+        public string name;
+        public string author;
+        public Genre genre;
+        public int year;
+        public int price;
+        List<Book> Library = new List<Book>();
+
+        static void Add
+        {
+            Console.WriteLine("Ввдите название произведения, фамилию автора, жанр из предложенных, год написания, цену за книгу");
+            for (int i = 0; i<Genre.length; i++)
+			{
+                Console.WriteLine(Genre[i]);
+			}
+
+    Library.add
+
+}
+
+    }
+}
