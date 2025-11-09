34b3438e4cd351d9f82b768eb25f0c2bf72349a9
SeyDeH <grekov.pavel07@gmail.com>
2025-11-10 01:50:19 +0300

    Сделал генератор предметов и описал зелье лечения

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -266,3 +266,53 @@ public class Player
         Console.WriteLine($"Ход: {TurnCount}");
     }
 }
+
+public static class ItemGenerator
+{
+    private static Random random = new Random();
+
+    private static readonly Weapon[] Weapons = new[]
+    {
+        new Weapon("Деревянный меч", 8, 10),
+        new Weapon("Железный меч", 12, 25),
+        new Weapon("Стальной меч", 16, 40),
+        new Weapon("Магический посох", 14, 35),
+        new Weapon("Легендарный клинок", 20, 60)
+    };
+
+    private static readonly Armor[] Armors = new[]
+    {
+        new Armor("Кожаная броня", 4, 15),
+        new Armor("Кольчуга", 6, 30),
+        new Armor("Латная броня", 8, 45),
+        new Armor("Магические доспехи", 7, 40),
+        new Armor("Легендарные доспехи", 10, 55)
+    };
+
+    public static Item GenerateRandomItem()
+    {
+        int itemType = random.Next(3);
+
+        switch (itemType)
+        {
+            case 0: 
+                return Weapons[random.Next(Weapons.Length)];
+            case 1: 
+                return Armors[random.Next(Armors.Length)];
+            case 2: 
+                return new HealthPotion();
+            default:
+                return new HealthPotion();
+        }
+    }
+}
+
+public class HealthPotion : Item
+{
+    public HealthPotion() : base("Зелье здоровья", 15) { }
+
+    public void Use(Player player)
+    {
+        player.Heal();
+    }
+}
