2ffdd984065282ab110d445571103e9d1459435b
SeyDeH <grekov.pavel07@gmail.com>
2025-11-10 01:44:14 +0300

    Создал абстрактный класс Item, и на его основе классы Weapon и Armor, также создал интерфейс IEnemy, на основе которого далее создам врагов

--- ISIP523_Myagkov/Program.cs
+++ ISIP523_Myagkov/Program.cs
@@ -1,1 +1,43 @@
-﻿
+using System.Numerics;
+
+public abstract class Item
+{
+    public string Name { get; protected set; }
+    public int Value { get; protected set; }
+
+    protected Item(string name, int value)
+    {
+        Name = name;
+        Value = value;
+    }
+}
+
+public class Weapon : Item
+{
+    public int Attack { get; private set; }
+
+    public Weapon(string name, int attack, int value) : base(name, value)
+    {
+        Attack = attack;
+    }
+}
+
+public class Armor : Item
+{
+    public int Defense { get; private set; }
+
+    public Armor(string name, int defense, int value) : base(name, value)
+    {
+        Defense = defense;
+    }
+}
+
+public interface IEnemy
+{
+    string Name { get; }
+    int Health { get; set; }
+    int Attack { get; }
+    int Defense { get; }
+    void SpecialAbility(Player player);
+    bool TrySpecialAbility();
+}
