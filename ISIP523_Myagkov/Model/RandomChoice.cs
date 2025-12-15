8ff17b9e0f4936c171cb57664a4dced9fe741f02
SeyDeH <grekov.pavel07@gmail.com>
2025-12-15 04:25:48 +0300

    Класс RandomChoice, EnemyFactory, Enemy

---
+++ ISIP523_Myagkov/Model/RandomChoice.cs
@@ -0,0 +1,22 @@
+﻿namespace Model
+{
+    public static class RandomChoice
+    {
+        private static Random random = new Random();
+
+        public static int Next(int maxValue)
+        {
+            return random.Next(maxValue);
+        }
+
+        public static int Next(int minValue, int maxValue)
+{
+            return random.Next(minValue, maxValue);
+        }
+
+        public static double NextDouble()
+    {
+            return random.NextDouble();
+        }
+    }
+}
